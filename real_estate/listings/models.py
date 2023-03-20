01da174d4fd8ecb5cacfb0a5eb00c2bc25504e6d
Brian Sai <brianwsai@gmail.com>
2023-03-19 19:13:10 -0700

    Created model for listings and migrated to db

--- real_estate/listings/models.py
+++ real_estate/listings/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Listing(models.Model):
-      title = models.CharField(max_length=150)
-      price = models.IntegerField()
-      num_bedrooms = models.IntegerField() 
-      num_bathrooms = models.IntegerField()
-      sq_foot = models.IntegerField()
-      address = models.CharField(max_length=100)
+  title = models.CharField(max_length=150)
+  price = models.IntegerField()
+  num_bedrooms = models.IntegerField() 
+  num_bathrooms = models.IntegerField()
+  sq_foot = models.IntegerField()
+  address = models.CharField(max_length=100)
       # image
+
+  def __str__(self):
+      return self.title
