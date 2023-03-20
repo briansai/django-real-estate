637a1c01662f0dc03702b0888e2b58a09411c91e
Brian Sai <brianwsai@gmail.com>
2023-03-19 17:03:51 -0700

    Initiated listings model

--- real_estate/listings/models.py
+++ real_estate/listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Listing(models.Model):
+      title = models.CharField(max_length=150)
+      price = models.IntegerField()
+      num_bedrooms = models.IntegerField() 
+      num_bathrooms = models.IntegerField()
+      sq_foot = models.IntegerField()
+      address = models.CharField(max_length=100)
+      # image
