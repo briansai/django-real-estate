9d173db86c10b1af2263457f9c907d34d817cb02
Brian Sai <brianwsai@gmail.com>
2023-03-25 12:55:03 -0700

    Created listings and listing html templates

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Listing
+from .forms import ListingForm
 
 # Create your views here.
 def listing_list(request):
@@ -8,3 +9,17 @@ def listing_list(request):
     "listings": listings
   }
   return render(request, "listings.html", context)
+
+def listing_retrieve(request, id):
+  listing = Listing.objects.get(id=id)
+  context = {
+    "listing": listing
+  }
+  return render(request, "listing.html", context)
+
+def listing_create(request):
+  form = ListingForm()
+  context = {
+    "form": form
+  }
+  return render(request, "ListingForm.html", context)
